d03c7d30826c25d057d661cfa4de3b3a2087da08
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2020-11-07 19:52:35 +0500

    Added template for ChoosePerson screen

---
+++ app/src/main/java/com/example/w3e/ChoosePerson.kt
@@ -0,0 +1,23 @@
+package com.example.w3e
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class ChoosePerson : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_person)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/w3e/GetUserInfo.kt
+++ app/src/main/java/com/example/w3e/GetUserInfo.kt
@@ -1,5 +1,6 @@
 package com.example.w3e
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_get_user_info.*
@@ -8,5 +9,10 @@ class GetUserInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_user_info)
+
+        next_btn.setOnClickListener {
+            val intent = Intent(this, ChoosePerson::class.java)
+            startActivity(intent)
+        }
     }
 }
