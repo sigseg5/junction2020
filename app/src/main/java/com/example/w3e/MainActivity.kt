923d1a2cb32273a99679e529c16827f672ec162f
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2020-11-07 17:45:08 +0500

    Setup for login screen

--- app/src/main/java/com/example/w3e/MainActivity.kt
+++ app/src/main/java/com/example/w3e/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.w3e
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        login_fb_btn.setOnClickListener {
+//            val intent = Intent(this, )
+        }
     }
 }
